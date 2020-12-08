40c73d30c2518d51ba8a4682bb80cd43d3079a0f
Azaria berry <azariaberry2019@outlook.com>
2020-12-08 17:53:29 -0500

    learned promo

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,3 +1,6 @@
 app.controller('MainController',['$scope', function($scope) {
-    $scope.title = 'Top Sellers in Books';
+    $scope.title = 'The Samuria Code';
+    $scope.promo = 'Honor, Courage, Honesty';
     }]);
+
+    //Both the controller MainController and the view index.html have access to $scope. This means we can use $scope to communicate between the controller and the view. In the controller, change the value of title to your own string.
